5dfcf36abd1f0d3360027ddd42bbaa2e39118552
Anthony Joaquin Bravo Berrio <anthony.bravo.berrio@gmail.com>
2020-04-06 14:58:27 -0500

    Cambio en el footer DEV

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/components/Footer.styl';
 
 const Footer = () => (
   <div className="Footer">
-    <p className="Footer-title">Platzi Store</p>
+    <p className="Footer-title">Platzi Store - DEV</p>
     <p className="Footer-copy">Todos los Izquierdos Reservados</p>
   </div>
 );
