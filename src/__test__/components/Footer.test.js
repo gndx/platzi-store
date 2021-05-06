5949f6f946bc92617249f1393895dd83e0ff07f0
Jair Avilés <hi@jairaviles.mx>
2021-05-05 20:58:18 -0500

    Add moduleNameMapper definition Add provider and style mock definitions Add Footer component test case

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,8 +3,12 @@ import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
 
 describe('Footer', () => {
+  const footer = mount(<Footer />);
+
   test('Should render component', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
+  });
+  test('Should render title', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
   });
 });
