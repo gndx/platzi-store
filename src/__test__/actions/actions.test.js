1e6a80b6a40d22c5016172c9b0bb3f1aafcfafa0
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-07-14 17:48:42 -0500

    :white_check_mark: [ADD] Redux actions tests

---
+++ src/__test__/actions/actions.test.js
@@ -0,0 +1,21 @@
+import actions from '../../actions';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('Actions from Redux', () => {
+  test('Should Add to cart', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'ADD_TO_CART',
+      payload,
+    };
+    expect(actions.addToCart(payload)).toEqual(expected);
+  });
+  test('Should Remove from cart', () => {
+    const payload = ProductMock;
+    const expected = {
+      type: 'REMOVE_FROM_CART',
+      payload,
+    };
+    expect(actions.removeFromCart(payload)).toEqual(expected);
+  });
+});
