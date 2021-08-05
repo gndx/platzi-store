5e0ceabf3df21ecdb1b22351bee8a5fd5b2deda1
David Almeida <djalmeida3@gmail.com>
2021-08-05 11:13:14 -0500

    Implementar provider mock

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+
+describe('<Header />', () => {
+    test('Render del componente Header', () => {
+        const header = shallow(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>,
+        );
+        expect(header.length).toEqual(1);
+    });
+    test('Render del Titulo', () => {
+        const header = mount(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>
+        );
+        expect(header.find(".Header-title").text()).toEqual("Platzi Store");
+    });
+});
