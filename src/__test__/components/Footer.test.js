18814e7ddbe957b2e0764f71d7bef94d2a3b2545
David Almeida <djalmeida3@gmail.com>
2021-08-05 10:20:53 -0500

    Crear mocks

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Footer from '../../components/Footer';
-import { test } from '@jest/globals';
 
 describe('<Footer/>', () => {
+    const footer = mount(<Footer />);
     test('Render del componente Footer', () => {
-        const footer = mount(<Footer />);
         expect(footer.length).toEqual(1);
+    });
+    test('Render del titulo', () => {
+        expect(footer.find('.Footer-title').text()).toEqual("Platzi Store");
     });
 });
