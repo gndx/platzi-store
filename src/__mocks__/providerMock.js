99963ff5a83df3e57909ab7960e90128d9f7ae43
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-29 15:03:21 -0600

    create Providr mock

---
+++ src/__mocks__/providerMock.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createBrowserHistory } from 'history'
+
+import initialState from '../initialState'
+import reducer from '../reducers'
+
+const store = createStore(reducer, initialState)
+const history = createBrowserHistory()
+const ProviderMock = props => {
+    return(
+        <Provider store={store}>
+            <Router history={history}>
+                {props.children}
+            </Router>
+        </Provider>
+    )
+}
+
+export default ProviderMock
