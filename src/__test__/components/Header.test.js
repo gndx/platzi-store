42c65a2f67a8117afc2377d80a4d5616bc2038e1
Brian Sanchez <brian.sanchez@inmediatum.com>
2021-12-29 15:03:41 -0600

    implement mockedProvider in order to test Header

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { mount, shallow } from 'enzyme'
+import ProviderMock from '../../__mocks__/providerMock'
+import Header from '../../components/Header'
+
+describe('<Header/>', () => {
+    
+    test('Render header', () => {
+        const header = shallow(
+            <ProviderMock>
+                <Header/>
+            </ProviderMock>
+        )
+        expect(header.length).toEqual(1)
+    })
+    test('Headers title render', () => {
+        const header = mount(
+            <ProviderMock>
+                <Header/>
+            </ProviderMock>
+        )
+        expect(header.find('.Header-title').text()).toEqual('Platzi Store')
+    })
+})
