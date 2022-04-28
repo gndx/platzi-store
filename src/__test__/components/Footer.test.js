f491803599a0366355c5745fb1238ff61809990a
Manuel Ricardo <manuelricardohurtado@gmail.com>
2022-04-28 14:43:03 -0400

    Test Update

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -3,3 +3,9 @@ import React from "react";
 import { mount } from 'enzyme';
 import Footer from "../../components/Footer";
 
+describe('<Footer />', () => {
+    test('Render del componente Footer', () => {
+        const footer = mount(<Footer />);
+        expect(footer.length).toEqual(1);
+    });
+}); 
